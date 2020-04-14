677794eeb2246ca46959c516bfa20c94a964ea7f
cleitonpin <55035738+cleitonpin@users.noreply.github.com>
2020-04-13 21:48:35 -0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -192,7 +192,7 @@ def main():
 
     port = int(os.environ.get("PORT", 5000))
     
-    app.run(host=args.host, port=port)
+    app.run(host='0.0.0.0', port=port)
   
 if __name__ == '__main__':
     main()
